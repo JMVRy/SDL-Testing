a067025509592e6e07b12a89992057fe5f59d1f1
JMVRy <srjr18@gmail.com>
2026-07-18 20:39:52 -0500

    added global namespace references

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,31 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+static ::SDL_Window *window = NULL;
+static ::SDL_Renderer *renderer = NULL;
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+::SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+    ::SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
 
-    if (!SDL_Init(SDL_INIT_VIDEO))
+    if (!::SDL_Init(SDL_INIT_VIDEO))
     {
-        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        ::SDL_Log("Couldn't initialize SDL: %s", ::SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer))
+    if (!::SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer))
     {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        ::SDL_Log("Couldn't create window/renderer: %s", ::SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    ::SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+::SDL_AppResult SDL_AppEvent(void *appstate, ::SDL_Event *event)
 {
     if (event->type == SDL_EVENT_QUIT)
     {
@@ -37,22 +37,22 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate)
+::SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    const double now = ((double)SDL_GetTicks()) / 1000.0;
-    const float red = (float)(0.5 + 0.5 * SDL_sin(now));
-    const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+    const double now = ((double)::SDL_GetTicks()) / 1000.0;
+    const float red = (float)(0.5 + 0.5 * ::SDL_sin(now));
+    const float green = (float)(0.5 + 0.5 * ::SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float)(0.5 + 0.5 * ::SDL_sin(now + SDL_PI_D * 4 / 3));
+    ::SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
 
-    SDL_RenderClear(renderer);
+    ::SDL_RenderClear(renderer);
 
-    SDL_RenderPresent(renderer);
+    ::SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
+void SDL_AppQuit(void *appstate, ::SDL_AppResult result)
 {
     // ...
 }
