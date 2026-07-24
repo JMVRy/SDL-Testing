88e0688faf89356c6927f073ff24ab307fca9adc
JMVRy <srjr18@gmail.com>
2026-07-24 10:57:19 -0500

    add GNU AGPLv3 license

--- run.bash
+++ run.bash
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+#    This file is part of SDL Testing.
+#
+#    SDL Testing is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    SDL Testing is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with SDL Testing.  If not, see <https://www.gnu.org/licenses/>.
+
 if [[ ! -f ./bin/output.x86_64 ]]; then
     echo "Error: output doesn't exist!" >&2
     echo "Run build.bash first!" >&2
